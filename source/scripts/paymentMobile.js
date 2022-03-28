0389176030ac7b74d040cef892b86abbb2b1c7b5
SoseD <sosedslim@gmail.com>
2022-03-28 12:29:53 +0300

    Add modal window

--- source/scripts/paymentMobile.js
+++ source/scripts/paymentMobile.js
@@ -2,31 +2,8 @@ import Accordion from "@/modules/containers/accordion/index";
 
 Accordion();
 
-// var stickyFooter = document.getElementsByClassName('page-footer--sticky')[0]
-
-
-// document.addEventListener('DOMContentLoaded', function(e) {
-//     document.addEventListener('scroll', function(e) {
-//         let documentHeight = document.body.scrollHeight;
-//         let currentScroll = window.scrollY + window.innerHeight;
-//         // When the user is [modifier]px from the bottom, fire the event.
-//         let modifier = 200; 
-//         if(currentScroll + modifier > documentHeight) {
-//             stickyFooter.classList.remove("page-footer--sticky");
-//         } else {
-//             stickyFooter.classList.add("page-footer--sticky");
-//         }
-//     })
-// })
-
-// $(window).scroll(function() {
-//     var scrollBottom = $(window).scrollTop() + $(window).height();
-//     if(scrollBottom > 100){
-//         console.log('321')
-//         $('.page-footer').removeClass('page-footer--sticky');
-//     } else{
-//         $('.page-footer').addClass('page-footer--sticky');
-//     }
+var VISIBLE = 'is-visible';
+var modalWindow = document.getElementById('modal-validation')
 
 $(window).scroll(function() {
     var scrollBottom = $(window).scrollTop() + $(window).height() >= $(document).height() - 80
@@ -45,3 +22,25 @@ $(window).scroll(function() {
         $('.go-back-link').removeClass('go-back-link--fixed');
     }
 });
+
+modalWindow.addEventListener("click", function(event) {
+    var target = event.target.className
+    if (target === 'modal__overlay' || target === 'button button--modal') {
+        closeModal(modalWindow);
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    var key = event.key; 
+    if (key === "Escape") {
+        closeModal(modalWindow);
+    }
+});
+
+// function showModal(e) {
+//     e.classList.add(VISIBLE)
+// }
+
+function closeModal(e) {
+    e.classList.remove(VISIBLE)
+}
